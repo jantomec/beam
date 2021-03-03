d47bfb7be50fd30e4bc4d2b57012bd8f6a975198
Jan Tomec <jan.tomec@gradri.uniri.hr>
2021-03-03 07:57:27 +0100

    Add class for element properties

--- structures.py
+++ structures.py
@@ -1,30 +1,50 @@
 import numpy as np
 from typing import Callable
 
-class Point:
-    def __init__(self, x):
-        self.x = np.array(x)
 
 class BeamIntegrationPoint:
     def __init__(
         self,
         pointsLocation: np.ndarray,
         weights: np.ndarray,
-        displacementInterpolation: Callable[
+        displacement_interpolation: Callable[
             [int, np.ndarray], np.ndarray
         ],
-        rotationInterpolation: Callable[
+        rotation_interpolation: Callable[
             [int, np.ndarray], np.ndarray
         ],
         nNodes: int
     ):
         self.loc = pointsLocation
         self.wgt = weights
-        self.Ndis = displacementInterpolation(
+        self.Ndis = displacement_interpolation(
             nNodes - 1,
             pointsLocation
         )
-        self.Nrot = rotationInterpolation(
+        self.Nrot = rotation_interpolation(
             nNodes - 1,
             pointsLocation
         )
+
+class BeamElementProperties:
+    def __init__(
+        self,
+        length: float,
+        area: float,
+        density: float,
+        elastic_modul: float,
+        shear_modul: float,
+        intertia_primary: float,
+        intertia_secondary: float,
+        inertia_torsion: float,
+        shear_coefficient: float
+    ):
+        self.L = length
+        self.A = area
+        self.rho = density
+        self.E = elastic_modul
+        self.G = shear_modul
+        self.I1 = intertia_primary
+        self.I2 = intertia_secondary
+        self.It = inertia_torsion
+        self.ks = shear_coefficient
