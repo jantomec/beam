a022d6aa6aa79097654f868c69896d2338874695
Jan Tomec <jan.tomec@gradri.uniri.hr>
2021-03-03 09:35:44 +0100

    Add skew and antiskew

--- mathematics.py
+++ mathematics.py
@@ -14,3 +14,23 @@ def normalized(a, axis=-1, order=2):
     l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
     l2[l2==0] = 1
     return a / np.expand_dims(l2, axis)
+
+def skew(r):
+    R = np.zeros(shape=(3,3))
+    R[0, 1] = - r[2]
+    R[0, 2] = r[1]
+    R[1, 0] = r[2]
+    R[1, 2] = - r[0]
+    R[2, 0] = - r[1]
+    R[2, 1] = r[0]
+    return R
+
+def antiskew(R):
+    r = np.zeros(shape=(3))
+    r[0] = R[2,1]
+    r[1] = R[0,2]
+    r[2] = R[1,2]
+    return r
+
+
+    
