5393d9107155d4483efed21e2595f2500e571444
Jan Tomec <jan.tomec@gradri.uniri.hr>
2021-03-03 09:45:07 +0100

    Add SO(3) exponential map

--- mathematics.py
+++ mathematics.py
@@ -31,3 +31,14 @@ def antiskew(R: np.ndarray) -> np.ndarray:
     r[1] = R[0,2]
     r[2] = R[1,2]
     return r
+
+def expSO3(R: np.ndarray) -> np.ndarray:
+    norm_R = np.linalg.norm(R) / np.sqrt(2)
+    if norm_R == 0:
+        return np.identity(3)
+
+    return (
+        np.identity(3) + 
+        np.sin(norm_R) * R / norm_R + 
+        (1 - np.cos(norm_R)) * R @ R / norm_R**2
+    )
