9f6631058c5b3b650f9e7c3345d4dd02c5a05f18
Jan Tomec <jan.tomec@gradri.uniri.hr>
2021-03-03 09:04:42 +0100

    Add constitutive and intertial matrix

--- structures.py
+++ structures.py
@@ -48,3 +48,14 @@ class BeamElementProperties:
         self.I2 = intertia_secondary
         self.It = inertia_torsion
         self.ks = shear_coefficient
+        self.C = np.zeros(shape=(6,6))
+        self.C[0,0] = self.E * self.A
+        self.C[1,1] = self.G * self.A
+        self.C[2,2] = self.G * self.A
+        self.C[3,3] = self.G * self.It
+        self.C[4,4] = self.E * self.I1
+        self.C[5,5] = self.E * self.I2
+        self.I = np.zeros(shape=(3,3))
+        self.I[0,0] = self.rho * (self.I1 + self.I2)
+        self.I[1,1] = self.rho * self.I1
+        self.I[2,2] = self.rho * self.I2
