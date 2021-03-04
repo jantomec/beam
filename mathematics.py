aab3d96a6525da41b0324219c88e5bf6671f10ce
Jan Tomec <jan.tomec@gradri.uniri.hr>
2021-03-04 13:50:44 +0100

    Debug array copying

--- mathematics.py
+++ mathematics.py
@@ -43,7 +43,7 @@ def expSO3(R: np.ndarray) -> np.ndarray:
     
 def rotvec_to_quat(rv):
     angle = np.linalg.norm(rv)
-    a = rv
+    a = np.array(rv)
     if angle != 0.0:
         a = a / angle
     q = np.zeros(shape=(4))
@@ -54,15 +54,17 @@ def rotvec_to_quat(rv):
     return q
 
 def quat_to_rotvec(q):
-    n = np.linalg.norm(q[:3])
+    qn = normalized(q)
+    qv = np.array(qn[:3])
+    n = np.linalg.norm(qv)
     rv = np.zeros(shape=(3))
     if n != 0.0:
-        angle = 2.0 * np.arctan2(n, q[3])
+        angle = 2.0 * np.arctan2(n, qn[3])
         if angle > 1.8 * np.pi:
             angle = angle - 2.0*np.pi
         if angle < -1.8 * np.pi:
             angle = angle + 2.0*np.pi
-        rv = angle * q[:3] / n
+        rv = angle * qv / n
     return rv
 
 def spurrier_quat_extraction(R):
@@ -116,10 +118,11 @@ def hamp(ql: np.ndarray, qr: np.ndarray) -> np.ndarray:
             ql[1]*qr[2] - ql[2]*qr[1])
     h[1] = (ql[3]*qr[1] - ql[0]*qr[2] +
             ql[1]*qr[3] + ql[2]*qr[0])
-    h[2] = (ql[3]*qr[2] + ql[0]*qr[1] +
+    h[2] = (ql[3]*qr[2] + ql[0]*qr[1] -
             ql[1]*qr[0] + ql[2]*qr[3])
-    h[3] = (ql[3]*qr[3] - ql[0]*qr[0] +
+    h[3] = (ql[3]*qr[3] - ql[0]*qr[0] -
             ql[1]*qr[1] - ql[2]*qr[2])
+
     return h
 
 def simo_dyn_linmap(Th: np.ndarray) -> np.ndarray:
@@ -131,8 +134,9 @@ def simo_dyn_linmap(Th: np.ndarray) -> np.ndarray:
     )
 
 def conjugate_quat(q: np.ndarray) -> np.ndarray:
-    q[:3] = -1.0 * q[:3]
-    return q
+    c = np.array(q)
+    c[:3] = -1.0 * c[:3]
+    return c
 
 def inverse_quat(q: np.ndarray) -> np.ndarray:
     n = np.linalg.norm(q) ** 2
@@ -144,7 +148,7 @@ def rotate(q: np.ndarray, v: np.ndarray) -> np.ndarray:
     Rotate a size (3) vector with quaternion
     """
     qv = np.zeros(shape=(4))
-    qv[:3] = v
+    qv[:3] = np.array(v)
     qv[3] = 0.0
     qinv = inverse_quat(q)
     q1 = hamp(q, qv)
